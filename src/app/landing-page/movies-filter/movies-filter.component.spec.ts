fc9092529cf58a0d32213b978ce5439d432bab8c
Johnny Alejandro <johnny.martinez@techandsolve.com>
2021-08-09 17:20:56 -0500

    test oki

--- src/app/landing-page/movies-filter/movies-filter.component.spec.ts
+++ src/app/landing-page/movies-filter/movies-filter.component.spec.ts
@@ -18,8 +18,4 @@ describe('MoviesFilterComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
--- src/app/landing-page/navbar-menu/navbar-menu.component.spec.ts
+++ src/app/landing-page/navbar-menu/navbar-menu.component.spec.ts
@@ -19,7 +19,4 @@ describe('NavbarMenuComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
